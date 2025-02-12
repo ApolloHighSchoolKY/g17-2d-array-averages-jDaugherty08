97a5adc1c9df17b9ccb4c4898ec392c4a63463ef
jDaugherty08 <joseph.daugherty@stu.daviess.kyschools.us>
2025-02-12 00:24:24 +0000

    :)

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -9,15 +9,49 @@ public class ArrayAverages{
   }
 
   public void rowAverages(){
-    
+    int rowAvg = 0;
+
+    for(int row = 0; row < ray.length; row++)
+    {
+      rowAvg = 0;
+      for(int col=0; col<ray[row].length; col++)
+      {
+        rowAvg += ray[row][col];
+      }
+      System.out.println("the average of row " + row + " is " + (double)rowAvg / ray.length);
+    }
   }
 
+  
+// Don't think this is exactly what we were supposed to do, but it works
   public void columnAverages(){
+    int colAvg = 0;
+
+    for(int col = 0; col < ray.length; col++)
+    {
+      colAvg = 0;
+      for(int row=0; row<ray.length; row++)
+      {
+        colAvg += ray[row][col];
+      }
+      System.out.println("the average of column " + col + " is " + (double)colAvg / ray.length);
+    }
 
   }
 
   public void arrayAverage(){
+    int rayAvg = 0;
+    int lazy = 0;
+    for(int[] row: ray)
+    {
+      for(int col: row)
+      {
+        rayAvg += col;
+        lazy++;
+      }
+    }
 
+    System.out.println("The average of the whole array is " + (double)rayAvg / lazy);
   }
 
   public String toString(){
@@ -25,5 +59,7 @@ public class ArrayAverages{
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+
+    return "";
   }
 }
--- Gee17.java
+++ Gee17.java
@@ -1,7 +1,30 @@
 public class Gee17{
-  public static void main(String[] args){
-    TwoDee twoDee = new TwoDee();
+  public static void main(String[] args)
+  {
 
+  int[][] ray = new int[7][7];
+
+  //populate the array with seqential numbers
+  /*
+   1 2 3  4  5  6  7
+   8 9 10 11 12 13 14
+   ...
+  */
+  int nums = 0;
+  for(int row =0; row<ray.length; row++)
+  {
+  for(int col = 0; col<ray[row].length; col++)
+  {
+    nums++;
+    ray[row][col] += nums;
+  }
+
+
+  }
+
+    ArrayAverages twoDee = new ArrayAverages(ray);
+
+    System.out.println(twoDee);
     twoDee.rowAverages();
     twoDee.columnAverages();
     twoDee.arrayAverage();
